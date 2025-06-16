96cc93018d7e98e03a96f50b822510b02e7f4b7f
Cookies173 <meet.kotadiya.mst21@itbhu.ac.in>
2025-06-16 21:20:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <main className="min-h-screen"> </main>
+      <footer className="bg-gray-300 py-10">
+        <div className="container mx-auto px-4 text-center text-black">
+          <p>© Cookies173</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
