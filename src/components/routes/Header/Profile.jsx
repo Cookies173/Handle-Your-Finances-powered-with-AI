7607269174ca4ce714ed9c13f2926563839d7669
Cookies173 <meet.kotadiya.mst21@itbhu.ac.in>
2025-06-21 19:33:39 +0530

    Day 4

--- src/components/routes/Header/Profile.jsx
+++ src/components/routes/Header/Profile.jsx
@@ -2,8 +2,36 @@ import React from "react";
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { useUser, useAuth } from "@clerk/clerk-react";
 
 function Profile(){
+
+    const {user, isLoaded} = useUser();
+    const { getToken } = useAuth();
+
+    useEffect(() => {
+        const syncUser = async () => {
+            if(isLoaded && user){
+                try{
+                    const token = await getToken();
+                    const res = await axios.post("https://penny-pilot-server.vercel.app/auth/user", null,{
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                console.log("User synced:", res.data);
+                }
+                catch(err){
+                    console.error("Failed to sync user:", err);
+                }
+            }
+        }; 
+        syncUser();
+    }, [isLoaded, user, getToken]);
+
     return (
         <div>
             <SignedOut>
