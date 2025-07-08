4fea5c870f360416b1ab1718f3debf23410fc63d
Cookies173 <meet.kotadiya.mst21@itbhu.ac.in>
2025-07-08 15:23:17 +0530

    register

--- src/components/routes/Authentication/Register.jsx
+++ src/components/routes/Authentication/Register.jsx
@@ -4,7 +4,7 @@ import {SignUp} from "@clerk/clerk-react";
 function Register(){
     return (
         <div className="flex justify-center pt-40">
-            <SignUp signUpForceRedirectUrl="/dasboard" />
+            <SignUp signUpForceRedirectUrl="/dashboard" />
         </div>
     );
 }
