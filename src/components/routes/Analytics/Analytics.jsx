ef63912484e8c85718c1874e1f82511c27c597e2
Cookies173 <meet.kotadiya.mst21@itbhu.ac.in>
2026-02-19 14:22:59 +0530

    Added overview + budget

--- src/components/routes/Analytics/Analytics.jsx
+++ src/components/routes/Analytics/Analytics.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
+import Header from "../Header/Header";
+import Footer from "../Header/Footer";
+import { BarLoader } from "react-spinners";
+const Budget = React.lazy(() => import("./Budget.jsx"));
+const Overview = React.lazy(() => import("./Overview.jsx"));
 
 function Analytics(){
+
     return (
     <div>
-        <h1>Hello World</h1>
+        <Header />
+        <div className="px-4 py-20">
+            <h1 className="text-6xl font-bold gradient-title mb-4">Analytics</h1>
+            <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#446B5C" />}>
+                <Budget />
+                <Overview />
+            </Suspense>
+        </div>
+        <Footer />
     </div>
     );
 }
--- src/components/routes/Dashboard/Dashboard.jsx
+++ src/components/routes/Dashboard/Dashboard.jsx
@@ -1,15 +1,11 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import Header from "../Header/Header.jsx";
 import Footer from "../Header/Footer.jsx";
 import { BarLoader } from "react-spinners";
 import { Toaster } from "@/components/ui/sonner";
-const Budget = React.lazy(() => import("./Budget.jsx"));
-const Overview = React.lazy(() => import("./Overview.jsx"));
 const AccountsGrid = React.lazy(() => import("./AccountsGrid.jsx"));
 
 function Dashboard(){
-
-    const [defaultProp, setDefaultProp] = useState([]);
 
     return(
         <div>
@@ -17,8 +13,6 @@ function Dashboard(){
             <div className="px-4 py-20">
                 <h1 className="text-6xl font-bold gradient-title mb-4">Dashboard</h1>
                 <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#446B5C" />}>
-                    {/* {defaultProp.length>0 && <Budget dprop={defaultProp} />} */}
-                    {/* <Overview /> */}
                     <AccountsGrid onNewDefault={setDefaultProp} />
                 </Suspense>
             </div>
